cb73181fe483a86b40f64b4819ea201e3848c039
Matteo Giliotti <matteogiliotti@gmail.com>
2024-01-04 19:02:17 -0500

    added requirements

--- cvweb/settings.py
+++ cvweb/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import dj_database_url
 import os
 from pathlib import Path
+import mimetypes
+
+mimetypes.add_type("text/javascript", ".js", True)
+mimetypes.add_type("text/css", ".css", True)
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
